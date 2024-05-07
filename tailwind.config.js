7e77e56d6929560847b605f7b24b52e1b482f967
Bruno <brunosampayo1040@gmail.com>
2024-05-07 15:42:31 -0300

    feat: add lib luciede react-icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,22 @@ export default {
   content: [ "./index.html",
   "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    colors:{
-      'dark-color-primary':'#0F0920',
-      'dark-color-secondary':'#221C3E',
-      'light-color':'#f1f1f1',
-      'lime-primary':'#16c586',
-      'light-color-primary':'#f2f0ff',
-      'light-color-secondary':'#b5b3bc'
-    },
+    
     fontFamily:{
       'custom-inconsolata':'Inconsolata',
       'custom-zenDots':'Zen Dots'
     },
-    extend: {},
+    extend: {
+      colors:{
+        'dark-color-primary':'#0F0920',
+        'dark-color-secondary':'#221C3E',
+        'light-color':'#f1f1f1',
+        'lime-primary':'#16c586',
+        'emerald-primary':'#03FCFC',
+        'light-color-primary':'#f2f0ff',
+        'light-color-secondary':'#b5b3bc'
+      },
+    },
   },
   plugins: [],
 }
